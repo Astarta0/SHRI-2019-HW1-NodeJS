022205e30f15c171e7d1ffdd71c2261c28433fc5
Anastasia Zaytseva <anastasia.zaytseva0@yandex.ru>
2019-09-21 03:54:09 +0300

    pages

--- src/client.js
+++ src/client.js
@@ -2,3 +2,27 @@ import hljs from 'highlight.js/lib/highlight';
 import python from 'highlight.js/lib/languages/python';
 hljs.registerLanguage('python', python);
 hljs.initHighlightingOnLoad();
+
+const dropdowns = document.querySelectorAll('.dropDown');
+for (const dropdown of dropdowns) {
+    let timer;
+    dropdown.addEventListener('mouseenter', e => {
+        clearTimeout(timer);
+        e.target.classList.add('dropDown_expanded');
+    });
+    dropdown.addEventListener('mouseleave', e => {
+        timer = setTimeout(() => e.target.classList.remove('dropDown_expanded'), 500);
+    });
+}
+
+const branchSelectors = document.querySelectorAll('.branchSelector');
+for (const branchSelector of branchSelectors) {
+    let timer;
+    branchSelector.addEventListener('mouseenter', e => {
+        clearTimeout(timer);
+        e.target.classList.add('branchSelector_expanded');
+    });
+    branchSelector.addEventListener('mouseleave', e => {
+        timer = setTimeout(() => e.target.classList.remove('branchSelector_expanded'), 500);
+    });
+}
